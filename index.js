c5d4abc10db11d50e16c7142e19723b4b2a747cc
iftakherone <54303402+iftakherone@users.noreply.github.com>
2021-11-18 18:29:34 +0600

    fixing bug

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ async function run() {
         app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await doctorsCollection.deleteOne(query);
+            const result = await reviewsCollection.deleteOne(query);
             res.json(result);
         })
 
